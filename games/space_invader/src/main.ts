6e5106d25425601049d136429f55d87370d2a1a0
Boom <pongsabutra@atato.com>
2023-06-15 18:15:09 +0700

    fixes ratio for mobile, remove margin add blackbackground

--- games/space_invader/src/main.ts
+++ games/space_invader/src/main.ts
@@ -9,7 +9,7 @@ const config: Phaser.Types.Core.GameConfig = {
 	width: SCREEN_WIDTH,
 	height: SCREEN_HEIGHT,
 	scale: {
-		mode: Phaser.Scale.HEIGHT_CONTROLS_WIDTH ,
+		mode: Phaser.Scale.WIDTH_CONTROLS_HEIGHT ,
 		autoCenter: Phaser.Scale.CENTER_BOTH
 	},
 	physics: {
