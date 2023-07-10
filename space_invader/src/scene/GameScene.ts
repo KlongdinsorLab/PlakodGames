642f64491ce95d32e9a96d6b6014a6e19d19fcbf
Boom <pongsabutra@atato.com>
2023-07-10 14:59:54 +0700

    fix build fail

--- space_invader/src/scene/GameScene.ts
+++ space_invader/src/scene/GameScene.ts
@@ -72,11 +72,10 @@ export default class GameScene extends Phaser.Scene {
     }
 
     create() {
-        const params = new Proxy(new URLSearchParams(window.location.search), {
-            get: (searchParams, prop: string) => searchParams.get(prop),
-        });
+//        const params = new Proxy(new URLSearchParams(window.location.search), {
+//            get: (searchParams, prop: string) => searchParams.get(prop),
+//        });
         // Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
-//        console.log(params)
 //      console.log(window.location.href)
 
         const {width, height} = this.scale
