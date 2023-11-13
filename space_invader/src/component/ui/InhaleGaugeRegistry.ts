974785cc8dd21735d8c140b7b723e91adf4367cb
Boom <pongsabutra@atato.com>
2023-11-13 17:56:31 +0700

    initial commit

--- space_invader/src/component/ui/InhaleGaugeRegistry.ts
+++ space_invader/src/component/ui/InhaleGaugeRegistry.ts
@@ -1,5 +1,5 @@
 //import CircleInhaleGauge from './CircleInhaleGauge'
-import CenterBarInhaleGauge from './CenterBarInhaleGauge'
+import CenterCircleInhaleGauge from './CenterCircleInhaleGauge'
 //import RingInhaleGauge from './RingInhaleGauge'
 //import BarInhaleGauge from './BarInhaleGauge'
 import InhaleGauge from './InhaleGauge'
@@ -13,7 +13,7 @@ export default class InhaleGaugeRegistry {
 
 	createbyDivision(division: number) {
 		this.holdbars = [...Array(division)].map(
-			(_, index: number) => new CenterBarInhaleGauge(this.scene, division, index),
+			(_, index: number) => new CenterCircleInhaleGauge(this.scene, division, index),
 		)
 	}
 
